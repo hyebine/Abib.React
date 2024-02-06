cf214716a62b8b06bc3561554fdcfa384d300689
hyebine <berryme015@gmail.com>
2024-02-06 18:00:39 +0900

    gnb 수정 swiper 작업

--- src/App.js
+++ src/App.js
@@ -5,19 +5,21 @@ import { Route, Routes } from 'react-router-dom';
 // component
 import Header from './component/layout/Header';
 import SwiperBanner from './component/swiper/Banner'
+import Category from './component/product/Category';
 // data
 import db from './data/db.json'
 // css
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './scss/style.scss'
+import './scss/Style.scss'
 
 
 function App() {
   return (
     <div className="App">
-      <Header datasrc={db.hd} ></Header>
+      <Header datasrc={db.hd}></Header>
       <Routes>
-        <Route path='/' element={<SwiperBanner></SwiperBanner>}></Route>
+        <Route path='/' element={<SwiperBanner swiperData={db.swiper.main}></SwiperBanner>}></Route>
+        <Route path='/' element={<Category categoryData={db.category} ></Category>}></Route>
       </Routes>
     </div>
   );
--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import { BsPerson, BsSearch } from "react-icons/bs";
 
-import '../../scss/header.scss'
+import '../../scss/Header.scss'
 
 
 function Header(props) {
 
   const [toggle, setToggle] = useState(false);
+
+
+  useEffect(() => {
+    const submenuli = document.querySelectorAll(".submenuis");
+    const gnb = document.querySelector(".navi .gnb")
+    submenuli.forEach((ele, idx) => {
+      ele.addEventListener('mouseenter', () => {
+        gnb.classList.add("showback")
+      })
+      ele.addEventListener('mouseleave', () => {
+        gnb.classList.remove("showback")
+      })
+
+    })
+  }, [])
 
   return (
     <header className='fixed-top'>
@@ -19,7 +34,7 @@ function Header(props) {
       </div>
 
       <div className='hd container d-flex align-items-center justify-content-between '>
-        <h1 className='logo position-relative zup'><Link to="/" className='d-block'><img src="/img/logo.png" alt="로고이미지" className='d-block' /></Link></h1>
+        <h1 className='logo w-0 position-relative zup'><Link to="/" className='d-block'><img src="/img/logo.png" alt="로고이미지" className='d-block' /></Link></h1>
 
         <button className={`gnb_btn position-relative d-lg-none ${toggle && 'act'} order-3`} onClick={() => { setToggle(!toggle) }}>
           <i></i>
@@ -28,25 +43,32 @@ function Header(props) {
         </button>
 
         <div className='navi d-lg-flex ms-auto ms-lg-0 flex-lg-grow-1 justify-content-between'>
-          <ul className='gnb d-none d-lg-flex flex-grow-1  justify-content-center'>
+
+
+
+
+          <ul className={`gnb d-none d-lg-flex flex-grow-1  justify-content-center ${toggle && 'act'} `}>
             {
               props.datasrc.navi.gnb.map((el, idx) => {
                 return (
-                  <li className='menu_li position-relative' key={`gnb_${idx}`}>
+                  <li className={`menu_li position-relative  ${el.sub.length > 0 ? 'submenuis' : ""}`} key={`gnb_${idx}`}>
                     <Link to={`/${el.href}`} className='menu_a'>
                       {el.nm}
                     </Link>
-                    <ul className='sub_ul zup position-absolute'>
-                      {
-                        el.sub.map((eel, iidx) => {
-                          return (
-                            <li key={iidx}>
-                              <Link to={`${eel.href}`}>{eel.nm}</Link>
-                            </li>
-                          )
-                        })
-                      }
-                    </ul>
+                    {
+                      el.sub.length > 0 &&
+                      <ul className='sub_ul zup position-absolute'>
+                        {
+                          el.sub.map((eel, iidx) => {
+                            return (
+                              <li key={iidx}>
+                                <Link to={`${eel.href}`}>{eel.nm}</Link>
+                              </li>
+                            )
+                          })
+                        }
+                      </ul>
+                    }
                   </li>
                 )
 
@@ -68,7 +90,7 @@ function Header(props) {
             </li>
             <li>
               <Link to="/">
-                <BsSearch className='d-block d-lg-none' size={25} />
+                <BsSearch className='d-block d-lg-none' size={23} />
                 <span className='d-none d-lg-block'>SEARCH</span>
               </Link>
             </li>
